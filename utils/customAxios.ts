005a05e7c3a468dcbb51539d6c9bd6f0a9b290af
LWC421 <lwc421@gmail.com>
2022-05-16 22:08:03 +0900

    오타 수정

--- utils/customAxios.ts
+++ utils/customAxios.ts
@@ -3,8 +3,8 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
-    ? process.env.BACKEND
-    : process.env.BACKEND,
+    ? process.env.NEXT_PUBLIC_BACKEND
+    : process.env.NEXT_PUBLIC_BACKEND,
   timeout: 10000,
   headers:{
     'Content-Type' : 'application/json'
