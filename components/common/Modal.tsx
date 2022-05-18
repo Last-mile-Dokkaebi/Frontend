271523a0079fcbff0a50ed5329684b6c43251aa0
LWC421 <lwc421@gmail.com>
2022-05-19 05:17:06 +0900

    모달창 변경

--- components/common/Modal.tsx
+++ components/common/Modal.tsx
@@ -10,9 +10,10 @@ interface DialogTypes {
 }
 
 const Modal = ({ delay = 0.5, children }: DialogTypes) => {
+  //Modal을 끌 것인지 켤 것인지 설정
   const { modalIsOpen } = useSelector((state: RootState) => state.system);
   const dispatch = useDispatch();
-  const [animation, setAnimation] = useState<any>(PopUp); //Type을 몰라서 일단 any로 해두었음
+  const [animation, setAnimation] = useState<any>(PopUp); // keyframe의 Type을 몰라서 일단 any로 해두었음
 
   const closeModal = useCallback(() => {
     //Delay를 주고 닫는다
@@ -52,11 +53,9 @@ const Background = styled.div<BackgroundTypes>`
   visibility: ${(props) => (props.open ? 'visible' : 'hidden')}
   z-index: 9999;
   position: fixed;
-  left: 0;
-  top: 0;
   width: 100vw;
   height: 100vh;
-  background: rgba(0, 0, 0, 0.25);
+  background: rgba(0, 0, 0, 0.15);
 
   display: flex;
   justify-content: center;
@@ -68,8 +67,11 @@ const Background = styled.div<BackgroundTypes>`
 const ModalWrapper = styled.div`
   z-index: 10000;
   position: fixed;
+  background-color: white;
+  box-shadow: 0px 0px 6px 2px rgba(0, 0, 0, 0.14), 0px 0px 12px 6px rgba(0, 0, 0, 0.1);
 `;
 
+//Modal이 켜질때 Animation
 const PopUp = keyframes`
   from{
     opacity: 0%;
@@ -81,6 +83,7 @@ const PopUp = keyframes`
   }
 `;
 
+//Modal이 꺼질때 Animation
 const PopDown = keyframes`
   from{
     opacity: 100%;
