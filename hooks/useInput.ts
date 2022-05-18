a3a4bbaacc9d3dc102dfdd2cb5ebe695bc67b44a
LWC421 <lwc421@gmail.com>
2022-05-19 06:42:16 +0900

    useInput 코드 리팩토링

--- hooks/useInput.ts
+++ hooks/useInput.ts
@@ -3,27 +3,15 @@ import React, { useState, useCallback } from 'react';
   제네릭(Generic)사용해서 다양한 타입 지원하게 변경
   https://velog.io/@edie_ko/TypeScript-Generic-%EC%A0%9C%EB%84%A4%EB%A6%AD-feat.-TypeScript-%EB%91%90-%EB%8B%AC%EC%B0%A8-%ED%9B%84%EA%B8%B0
 */
-const useInput = <T>(initialState: T, func?: (e: any)=>T): [T, (e: any)=>void] => {
+const useInput = <T>(initialState: T, callback?: (e: any)=>T): [T, (e: any)=>void] => {
   const [value, setValue] = useState<T>(initialState)
   
-  let handler;
-
-  //값에 대한 처리절차인 func가 주어질 경우 해당 절차에 따라 처리
-  if(func !== undefined){
-    handler = useCallback((e: any) => {
-      const targetValue = func(e)
-      setValue(targetValue)
-    }, [])
-  }
-  else{ 
-    //func가 주어져있지 않으면 단순 changeEvent라고 가정
-    //changeEvent가 아닐 경우 func를 주어줘서 적절하게 처리 하도록
-    handler = useCallback((e: any) => {
-      //Generic Type으로 type casting
-      const targetValue = e.target.value as unknown as T
-      setValue(targetValue)
-    }, [])
-  }
+  //callback이 주어질 경우 callback에 맞추어 처리
+  //주어지지 않을 경우 e.target.value로 간주
+  const handler = useCallback((e: any) => {
+    const targetValue = callback ? callback(e) :  e.target.value as unknown as T
+    setValue(targetValue)
+  }, [])
 
   return [value, handler]
 }
