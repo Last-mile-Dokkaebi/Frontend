c56778642d841979f824256a101e1255baea4b1d
LWC421 <lwc421@gmail.com>
2022-05-19 19:21:40 +0900

    로그인 로그아웃 및 접근권한

--- components/layout/index.ts
+++ components/layout/index.ts
@@ -3,6 +3,7 @@ export {default as AppLayout} from './AppLayout'
 export {default as Header} from './AppLayout'
 export {default as MemberLayout} from './AppLayout'
 export {default as NavBar} from './AppLayout'
+export {default as FullPageLoading} from '../layout/FullPageLoading'
 
 /*
   추후 사용시에는 import {AppLayout, Header} from 'components/layout
