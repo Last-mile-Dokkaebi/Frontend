29c5166d2c182a7564bbb5247febff61ecfd3442
hayoungzzzzz <hayoung474@gmail.com>
2022-05-22 23:20:16 +0900

    여러개의 마커 표시 (더미데이터)

--- pages/scooter/index.tsx
+++ pages/scooter/index.tsx
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import { AppLayout } from 'components/layout';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import { Button } from 'components/common';
+import Router from 'next/router';
 
 const index: NextPage = () => {
   const [position, setPosition] = useState<{ lat: number; lng: number }>({ lat: 0, lng: 0 });
@@ -12,12 +13,25 @@ const index: NextPage = () => {
     // api통신해서 불러오는 부분 작성 필요
     setPosition({ lat: dummyData.lat, lng: dummyData.lng });
   }, []);
+  const rideStart = () => {
+    Router.push('/scooter/ride');
+  };
 
   return (
     <AppLayout>
       <MapWrapper>
-      <Map center={position} style={{ width: '100%', height: '100%' }} level={5}>
-          <MapMarker
+        <Map center={position} style={{ width: '100%', height: '100%' }} level={5}>
+          {dummayDatas.map((position, index) => (
+            <MapMarker
+              key={index}
+              position={{ lat: position.lat, lng: position.lng }} // 마커를 표시할 위치
+              image={{
+                src: '/assets/img/marker2.png',
+                size: { width: 35, height: 40 },
+              }}
+            />
+          ))}
+          {/* <MapMarker
             position={position}
             image={{
               src: '/assets/img/marker2.png',
@@ -25,11 +39,10 @@ const index: NextPage = () => {
             }}
           >
             <div style={{ color: '#000' }}>Hello World!</div>
-          </MapMarker>
+          </MapMarker> */}
         </Map>
       </MapWrapper>
-      <Button>탑승 시작</Button>
-
+      <Button onClick={rideStart}>탑승 시작</Button>
     </AppLayout>
   );
 };
@@ -40,7 +53,18 @@ const dummyData: ScooterState = {
   soc: 30,
 };
 
-
+const dummayDatas: Array<ScooterState> = [
+  {
+    lat: 36.144765,
+    lng: 128.392134,
+    soc: 30,
+  },
+  {
+    lat: 36.14694,
+    lng: 128.3909,
+    soc: 77,
+  },
+];
 
 const MapWrapper = styled.div`
   width: 100%;
