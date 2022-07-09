36007b949c189d9baf3b8d4cf246264f2f69a7f5
LWC421 <lwc421@gmail.com>
2022-07-10 04:19:00 +0900

    주행중 맵 제작

--- utils/processing.ts
+++ utils/processing.ts
@@ -1,4 +1,9 @@
-const DateToString = (date:Date) => {
+/**
+ * 날짜 형식을 문자형식으로 바꾸어 반환합니다
+ * @param date String으로 반환되기를 원하는 날짜
+ * @returns 'YYYY-MM-DD'형태의 String
+ */
+const DateToString = (date:Date): string => {
   const dateYear  = date.getFullYear();
   const dateMonth = date.getMonth();
   const dateDay   = date.getDate();
@@ -8,6 +13,18 @@ const DateToString = (date:Date) => {
   return stringDate;
 }
 
+/**
+ * 날짜(시간을 포함한) 형식을 문자형식으로 바꾸어 반환합니다.
+ * @param date String으로 변환되기를 원하는 시간
+ * @returns 'HH:MM'형태의 String
+ */
+const TimeToString = (date:Date): string => {
+  const stringTime = `${date.getHours().toString().padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`
+
+  return stringTime;
+}
+
 export{
   DateToString,
+  TimeToString
 }
