3ab31481d1bfbc5f7dcec3b159e6d00d90e8fcb8
LWC421 <lwc421@gmail.com>
2022-11-14 15:17:43 +0900

    [fix]기능 없는 로그인 삭제

--- pages/member/index.tsx
+++ pages/member/index.tsx
@@ -9,33 +9,32 @@ import styled from 'styled-components';
 import { RiKakaoTalkFill } from 'react-icons/ri';
 import { AiFillFacebook } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
-import Router from "next/router"
+import Router from 'next/router';
 
-const Home:NextPage = () => {
-
-  const onClickLogo = ()=>{
-    Router.push("/member");
-  }
-  const onClickKaKao = ()=>{
-    alert("카카오톡 로그인 준비중"); // 나중에 alert말고 모달창을 준비하면 좋을듯. 토스트팝업이나
-  }
-  const onClickFacebook = () =>{
-    alert("페이스북 로그인 준비중");
-  }
-  const onClickGoogle = () =>{
-    alert("구글 로그인 준비중");
-  }
-  const onClickNormalLogin = ()=>{
-    Router.push("/member/login");
-  }
+const Home: NextPage = () => {
+  const onClickLogo = () => {
+    Router.push('/member');
+  };
+  const onClickKaKao = () => {
+    alert('카카오톡 로그인 준비중'); // 나중에 alert말고 모달창을 준비하면 좋을듯. 토스트팝업이나
+  };
+  const onClickFacebook = () => {
+    alert('페이스북 로그인 준비중');
+  };
+  const onClickGoogle = () => {
+    alert('구글 로그인 준비중');
+  };
+  const onClickNormalLogin = () => {
+    Router.push('/member/login');
+  };
 
   return (
     <MemberLayout>
-      <ImageWrapper >
+      <ImageWrapper>
         <Image src={'/assets/img/도깨비메인.PNG'} />
       </ImageWrapper>
       <ButtonWrapper>
-        <LoginButton onClick={onClickKaKao} bgcolor="#FEE500" color="#000000">
+        {/* <LoginButton onClick={onClickKaKao} bgcolor="#FEE500" color="#000000">
           <RiKakaoTalkFill size="20" color="#000000" />
           카카오로 시작하기
         </LoginButton>
@@ -46,7 +45,7 @@ const Home:NextPage = () => {
         <LoginButton onClick={onClickGoogle} bgcolor="white" color="#000000" style={{ border: 'solid 1px #00000025' }}>
           <FcGoogle size="20" />
           구글 로그인
-        </LoginButton>
+        </LoginButton> */}
         <LoginButton onClick={onClickNormalLogin} bgcolor="#77b8c0" color="#ffffff">
           일반 로그인
         </LoginButton>
@@ -55,11 +54,10 @@ const Home:NextPage = () => {
   );
 };
 
-
 const ImageWrapper = styled.div`
   width: 10rem;
   overflow: hidden;
-  margin:50px auto;
+  margin: 50px auto;
 `;
 const Image = styled.img`
   width: 100%;
@@ -71,7 +69,7 @@ const ButtonWrapper = styled.div`
   flex-direction: column;
   align-items: center;
   margin-top: 50px;
-  padding:1rem;
+  padding: 1rem;
 `;
 
 interface LoginButtonTypes {
@@ -89,12 +87,12 @@ const LoginButton = styled.button<LoginButtonTypes>`
   height: 50px;
   cursor: pointer;
   padding: 0.5rem 0.25rem 0.5rem 0.25rem;
-  margin:0.5rem 0 0.5rem 0;
+  margin: 0.5rem 0 0.5rem 0;
   & * {
     margin-right: 0.5rem;
   }
-  width:100%;
-  border:none;
+  width: 100%;
+  border: none;
 `;
 
 export default Home;
